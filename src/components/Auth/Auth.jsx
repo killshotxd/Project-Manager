f7ee4b3c8aeb29877b88c1050e67cb28fdd66a46
killshotxd <hassanansari211@gmail.com>
2022-08-20 09:42:07 +0530

    login/signup 2

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import InputControl from "../InputControl/InputControl";
+import { Link, useNavigate } from "react-router-dom";
 import styles from "./Auth.module.css";
 const Auth = (props) => {
   const isSignUp = props.signUp ? true : false;
+  const navigate = useNavigate();
 
   return (
     <div className={styles.container}>
-      <p className={styles.smallLink}>{"< Back to Home"}</p>
+      <p className={styles.smallLink}>
+        <Link to="/">{"< Back to Home"}</Link>
+      </p>
       <form className={styles.form}>
         <p className={styles.heading}>{isSignUp ? "SignUp" : "Login"}</p>
 
-        <InputControl />
+        <InputControl label="Name" placeholder="Enter your name" />
+        <InputControl label="Email" placeholder="Enter your email" />
+        <InputControl label="Password" placeholder="Enter your password" />
+
+        <button>{isSignUp ? "SignUp" : "Login"}</button>
       </form>
     </div>
   );
--- src/components/InputControl/InputControl.jsx
+++ src/components/InputControl/InputControl.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { EyeOff, Eye } from "react-feather";
 import styles from "./InputControl.module.css";
 const InputControl = ({ label, isPassword, ...props }) => {
+  const [isVisible, setIsVisible] = useState(true);
+
   return (
     <div className={styles.container}>
       {label && <label>{label}</label>}
-      <input type="text" {...props} />
+      <div className={styles.inputContainer}>
+        <input
+          type={isPassword ? (isVisible ? "text" : "password") : "text"}
+          {...props}
+        />
+        {isPassword && (
+          <div className={styles.icon}>
+            {isVisible ? (
+              <EyeOff onClick={() => setIsVisible((prev) => !prev)} />
+            ) : (
+              <Eye onClick={() => setIsVisible((prev) => !prev)} />
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
