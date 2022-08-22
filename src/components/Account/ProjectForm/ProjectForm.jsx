b22dafd038cbecc575c4ec9aa4e5211958044482
killshotxd <hassanansari211@gmail.com>
2022-08-22 17:18:11 +0530

    Modal Form

--- src/components/Account/ProjectForm/ProjectForm.jsx
+++ src/components/Account/ProjectForm/ProjectForm.jsx
@@ -1,13 +1,67 @@
 import React from "react";
+import InputControl from "../../InputControl/InputControl";
 import Modal from "../../Modal/Modal";
 import styles from "./ProjectForm.module.css";
 const ProjectForm = (props) => {
+  // --------States----------------
+
+  const [values, setValues] = useState({
+    thumbnail: defaults?.thumbnail || "",
+    title: defaults?.title || "",
+    overview: defaults?.overview || "",
+    github: defaults?.github || "",
+    link: defaults?.link || "",
+    points: defaults?.points || ["", ""],
+  });
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const [imageUploadStarted, setImageUploadStarted] = useState(false);
+
+  const [imageUploadProgress, setImageUploadProgress] = useState(0);
+
+  const [submitButtonDisabled, setSetSubmitButtonDisabled] = useState(false);
+
+  //   ---------------------------------------------
   return (
-    <div>
-      <Modal onClose={() => (props.onClose ? props.onClose() : "")}>
-        <h1>Hello</h1>
-      </Modal>
-    </div>
+    <Modal onClose={() => (props.onClose ? props.onClose() : "")}>
+      <div className={styles.container}>
+        <div className={styles.inner}>
+          <div className={styles.left}>
+            <div className={styles.image}>
+              <img
+                src="https://cdn.iconscout.com/icon/premium/png-256-thumb/developer-5-338076.png"
+                alt="Thumbnail"
+              />
+              <p>
+                <span>40%</span> Uploaded...
+              </p>
+            </div>
+
+            <InputControl label="Github" />
+            <InputControl label="Deployed/Live Link" />
+          </div>
+          <div className={styles.right}>
+            <InputControl label="Project Name" />
+            <InputControl label="Project Overview" />
+
+            <div className={styles.description}>
+              <div className={styles.top}>
+                <p className={styles.title}>Project Description</p>
+                <p className={styles.link}>+ Add point</p>
+              </div>
+
+              <InputControl />
+              <InputControl />
+            </div>
+          </div>
+        </div>
+        <div className={styles.footer}>
+          <p className={styles.footer}>Cancel</p>
+          <button className="button">Submit</button>
+        </div>
+      </div>
+    </Modal>
   );
 };
 
